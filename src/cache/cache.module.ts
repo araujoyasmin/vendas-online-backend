19f4c169e1e4864fb7303500b9f5322add70edfa
araujoyasmin <yasaraujo@icloud.com>
2023-04-17 02:22:50 -0300

    feat: create cache module

---
+++ src/cache/cache.module.ts
@@ -0,0 +1,11 @@
+import { CacheModule as CacheModuleNest,  Module } from '@nestjs/common';
+import { CacheService } from './cache.service';
+
+@Module({
+  imports:[CacheModuleNest.register({
+    ttl:9000000,
+  })],
+  providers: [CacheService],
+  exports: [CacheService]
+})
+export class CacheModule {}
--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CityEntity } from './entities/city.entity';
 import { Repository } from 'typeorm';
 import { Cache } from 'cache-manager';
+import { CacheService } from 'src/cache/cache.service';
 
 @Injectable()
 export class CityService {
@@ -10,27 +11,38 @@ export class CityService {
 constructor(
     @InjectRepository(CityEntity)
     private readonly cityRepository: Repository<CityEntity>,
-    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private readonly cacheService: CacheService,
+    // @Inject(CACHE_MANAGER) private cacheManager: Cache,
 ){}
 
 async getAllCitiesById(stateId): Promise<CityEntity[]>{
-    const citiesCache: CityEntity[] = await this.cacheManager.get(
+
+    return this.cacheService.getCache<CityEntity[]>(
         `state_${stateId}`,
+        () => this.cityRepository.find({
+            where: {
+                stateId,
+            },
+        }),
     );
 
-    if(citiesCache){
-        return citiesCache;
-    }
+    // const citiesCache: CityEntity[] = await this.cacheManager.get(
+    //     `state_${stateId}`,
+    // );
 
-    const cities = await this.cityRepository.find({
-        where:{
-            stateId
-        }
-    });
+    // if(citiesCache){
+    //     return citiesCache;
+    // }
 
-    await this.cacheManager.set(`state_${stateId}`, cities);
+    // const cities = await this.cityRepository.find({
+    //     where:{
+    //         stateId
+    //     }
+    // });
 
-    return cities;
+    // await this.cacheManager.set(`state_${stateId}`, cities);
+
+    // return cities;
      
    
 }
