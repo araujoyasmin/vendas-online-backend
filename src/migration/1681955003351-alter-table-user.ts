bef6f7b61c54bd1063e436ad68686b2b0022415e
araujoyasmin <yasaraujo@icloud.com>
2023-04-19 22:51:24 -0300

    feat: insert validation in email duplicated

---
+++ src/migration/1681955003351-alter-table-user.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class AlterTableUser1681955003351 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+            alter table public.user add unique(email);
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+           
+        `);
+    }
+
+}
