4d4ca4d5b82195bbcea066f6d1d259c6ab7b6f37
araujoyasmin <yasaraujo@icloud.com>
2023-05-11 02:19:43 -0300

    feat: create order product entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,8 @@ import { CartModule } from './cart/cart.module';
 import { CartProductModule } from './cart-product/cart-product.module';
 import { PaymentStatusModule } from './payment-status/payment-status.module';
 import { PaymentModule } from './payment/payment.module';
+import { OrderModule } from './order/order.module';
+import { OrderProductModule } from './order-product/order-product.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
@@ -32,7 +34,7 @@ import { PaymentModule } from './payment/payment.module';
       migrations: [`${__dirname}/migration/{.ts,*.js}`],
       migrationsRun: true
     })
-    ,UserModule, StateModule, CityModule, AddressModule, CacheModule, AuthModule, JwtModule, CategoryModule, ProductModule, CartModule, CartProductModule, PaymentStatusModule, PaymentModule],
+    ,UserModule, StateModule, CityModule, AddressModule, CacheModule, AuthModule, JwtModule, CategoryModule, ProductModule, CartModule, CartProductModule, PaymentStatusModule, PaymentModule, OrderModule, OrderProductModule],
   controllers: [],
   providers: [
     {
---
+++ src/order/entities/order.entity.ts
@@ -0,0 +1,27 @@
+
+import { Column, Entity, CreateDateColumn,PrimaryGeneratedColumn, OneToMany, ManyToOne, JoinColumn } from "typeorm";
+
+@Entity({ name: 'order'})
+export class OrderEntity {
+    @PrimaryGeneratedColumn('rowid')
+    id: number;
+
+    @Column({ name:'user_id', nullable:false})
+    userId: number;
+
+    @Column({ name:'address_id', nullable:false})
+    addressId: number;
+
+    @Column({ name:'date', nullable:false})
+    date: Date;
+
+    @Column({ name:'payment_id', nullable:false})
+    paymentId: number;
+
+    @CreateDateColumn({ name: 'created_at'})
+    createdAt: Date;
+
+    @CreateDateColumn({ name: 'updated_at'})
+    updatedAt: Date;
+
+}
